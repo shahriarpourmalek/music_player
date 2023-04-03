c3762167b088c7cda1ad9624fc6f1d65c250c393
shahriar pourmalek <shahriar.purmalek@gmail.com>
2023-04-03 12:23:59 +0430

    dfdf

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -15,3 +15,14 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
+import Echo from "laravel-echo"
+import Pusher  from "pusher-js";
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: 'your-pusher-key',
+    wsHost: window.location.hostname,
+    wsPort: 6001,
+    forceTLS: false,
+    disableStats: true,
+});
